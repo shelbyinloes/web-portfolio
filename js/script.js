afea44ed93eea031d9fd49a95707cbb9dde2773a
Shelby Inloes <shelbyinloes@gmail.com>
2018-08-23 20:04:19 -0500

    new background image + udemy first assignment practice

--- js/script.js
+++ js/script.js
@@ -1,18 +1,37 @@
+// new Vue({
+//     el: '#app',
+//     data: {
+//         title: "Shelby Danielle",
+//         link: 'https://google.com', 
+//         finishedLink: '<a href="http://google.com">Google</a>',
+//         yourName: "your-name-here"
+//     },
+//     methods: {
+//         sayHello: function() {
+//             this.title = 'Shelby'
+//             return this.title;
+//         },    
+//         changeYourName: function(event){
+//             this.yourName = event.target.value;
+//         }
+//     }
+// });
 new Vue({
-    el: '#app',
+    el: '#exercise',
     data: {
-        title: "Shelby Danielle",
-        link: 'https://google.com', 
-        finishedLink: '<a href="http://google.com">Google</a>',
-        yourName: "your-name-here"
+        name: "Shelby",
+        age: 25,
+        imgURL: "https://scontent-iad3-1.cdninstagram.com/vp/7cd90046991691bde97a5274ed28c0a0/5BE608DC/t51.2885-19/s150x150/13391116_1734249263527697_648025196_a.jpg"
     },
     methods: {
-        sayHello: function() {
-            this.title = 'Shelby'
-            return this.title;
-        },    
-        changeYourName: function(event){
-            this.yourName = event.target.value;
+        multiplyAge: function() {
+            return this.age * 3;
+        },
+        mathRandom: function() {
+            return Math.random();
+        },
+        changeName: function(event){
+            this.name = event.target.value;
         }
     }
 });
